8b6c7917fe3dfc0b6ae40ae21b49eb775c6bb242
youz2me <kynhun20@gachon.ac.kr>
2024-09-24 02:22:43 +0900

    fix: 1주차 검증 코드 오류 수정

--- week1/validate.sh
+++ week1/validate.sh
@@ -1,12 +1,12 @@
 current_branch=$(git branch --show-current)
+week1_committed=false
+ignore_committed=false
 
 if [[ "$current_branch" == week1-* ]]; then
     echo "   ✅ 브랜치 이름이 올바릅니다: $current_branch"
 else
     echo -e "   ❌ 브랜치 이름이 잘못되었습니다.\n   현재 브랜치 이름은 $current_branch 이며, week1-닉네임 형식이어야 해요!"
     exit 1
-fi
-
 fi
 
 if [ -f "week1.md" ]; then
@@ -45,14 +45,25 @@ else
     exit 1
 fi
 
-last_commit_files=$(git show --pretty="" --name-only HEAD)
+if git log --pretty=format:"%H" -- week1.md | grep -q '.'; then
+    week1_committed=true
+fi
 
-if [[ "$last_commit_files" == "week1.md" ]]; then
-    echo "   ✅ week1.md 파일만 커밋되었습니다."
+if git log --pretty=format:"%H" -- ignore.md | grep -q '.'; then
+    ignore_committed=true
+fi
+
+if $week1_committed && ! $ignore_committed; then
+    echo "   ✅ week1.md 파일이 커밋되었습니다.\n   ✅ ignore.md 파일이 정상적으로 .gitignore에 추가되었습니다."
 else
-    echo "   ❌ 커밋이 잘못되었습니다.\n   week1.md 파일만 커밋해주세요."
+    if ! $week1_committed; then
+        echo "   ❌ week1.md 파일이 커밋되지 않았습니다."
+    fi
+    if $ignore_committed; then
+        echo "   ❌ ignore.md 파일이 커밋되었습니다. ignore.md 파일은 커밋되지 않아야 합니다."
+    fi
     exit 1
-fi       
+fi
 
 echo "=================================================================="
 echo "✅ 모든 과제가 확인되었습니다!"
