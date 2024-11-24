4573f37ec54b91c6aaa2bdc61288252fbfdf34f7
youz2me <kynhun20@gachon.ac.kr>
2024-11-25 03:38:48 +0900

    feat: 10주차 과제 설명 및 검증 추가

--- week10/info.sh
+++ week10/info.sh
@@ -1,53 +1,9 @@
-# fast-forward-merge 브랜치 생성 및 설정
-git branch -D ff-merge 2> /dev/null # 기존 브랜치 삭제 (있을 경우)
-if git switch -c ff-merge; then
-    echo "✅ ff-merge 브랜치가 생성되었습니다."
-else
-    echo "❌ ff-merge 브랜치 생성에 실패했습니다."
-    exit 1
-fi
-
-echo "Updated content for fast-forward merge" > ff-file.txt
-git add ff-file.txt
-git commit -m "Add ff-file.txt for fast-forward merge"
-
-
-# threeway-merge 브랜치 생성 및 설정
-git branch -D threeway-merge 2> /dev/null # 기존 브랜치 삭제 (있을 경우)
-if git switch -c threeway-merge; then
-    echo "✅ threeway-merge 브랜치가 생성되었습니다."
-else
-    echo "❌ threeway-merge 브랜치 생성에 실패했습니다."
-    exit 1
-fi
-
-echo "Some content for threeway merge" > threeway-file.txt
-git add threeway-file.txt
-git commit -m "Add threeway-file.txt for three-way merge"
-
-
-# pull-test 브랜치 생성 및 설정
-git branch -D pull-test 2> /dev/null # 기존 브랜치 삭제 (있을 경우)
-if git switch -c pull-test; then
-    echo "✅ pull-test 브랜치가 생성되었습니다."
-else
-    echo "❌ pull-test 브랜치 생성에 실패했습니다."
-    exit 1
-fi
-
-echo "Some content for pull test" > pull-file.txt
-git add pull-file.txt
-git commit -m "Add pull-file.txt for pull test"
-
-
-echo "✅ 모든 브랜치가 설정되었습니다."
-
 echo "===============================10주차 과제==============================="
 echo "0. 먼저 'week10-(본인 닉네임)' 브랜치를 생성하고 해당 브랜치로 이동해주세요! (예: week10-zimging)"
-echo "1. 현재 브랜치에서 fast-forward-merge 브랜치 내용을 fast-forward로 병합해주세요."
-echo "2. 'week10-(본인 닉네임)' 브랜치로 이동 후, 해당 브랜치에서 파일을 생성하거나 수정해 커밋을 해주세요."
-echo "3. 현재 브랜치에서 threeway-merge 브랜치를 3-way 머지해보세요. 충돌을 해결해주세요."
-echo "4. pull-test 브랜치를 원격 저장소에서 pull 해보세요. 충돌을 해결한 후 커밋해주세요."
-echo "5. 모든 실습 과정을 캡쳐한 후, 해당 내용을 week10 브랜치에 push한 뒤 Pull Request를 만들어주세요!"
+echo "1. 현재 브랜치에서 merge 명령어를 이용해 ff-merge 브랜치 내용을 현재 브랜치로 병합해주세요."
+echo "2. 현재 브랜치에서 merge 명령어를 이용해 threeway-merge 브랜치 내용을 현재 브랜치로 병합해주세요. 병합 중 충돌이 발생했을 경우 threeway-merge 브랜치의 내용만 남도록 충돌을 해결해주세요."
+echo "3. 현재 브랜치에서 pull 명령어를 이용해 pull-test 브랜치를 현재 브랜치로 병합해주세요. 병합 중 충돌이 발생했을 경우 두 브랜치의 내용이 모두 남도록 충돌을 해결해주세요."
+echo "4. 모든 실습이 완료되었다면 validate.sh 명령어를 이용해 위의 내용을 올바르게 실습했는지 확인해주세요!"
+echo "5. \"모든 과제가 확인되었습니다\"라는 메시지를 확인하셨다면, 캡쳐하신 후 'week10-(본인 닉네임)' 브랜치를 push해주세요!"
+echo "6. 양식에 맞춰 Pull Request를 작성해주세요!"
 echo "======================================================================="
-
